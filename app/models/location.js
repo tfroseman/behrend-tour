a1e5d898cec89d243e620d2d8c4bb7f982991d89
tfroseman <roseman.tom@gmail.com>
2018-06-28 21:40:29 -0600

    redoing schema
    the schema needs a bit more work to be robust like an index and file ref for things like images or videos

--- app/models/location.js
+++ app/models/location.js
@@ -1,17 +1,16 @@
 // load the things we need.
 var mongoose = require('mongoose');
-
+var Schema = mongoose.Schema;
 
 // define the schema for our location model.
-var locationSchema = mongoose.Schema({
-  local           :{
+var locationSchema = new Schema({
+    id            :Number,
     name          :String,
     description   :String,
-  },
 });
 
-// methods ======================
 
+var LocationModel = mongoose.model('location', locationSchema );
 
-// create the model for users and expose it to our app.
-module.exports = mongoose.model('User', userSchema);
+//exports.Location = LocationModel;
+module.exports = LocationModel;
