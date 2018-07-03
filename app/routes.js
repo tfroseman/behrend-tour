b55afd60369fe50bb405822bcaeed2bf8985df81
amr6163 <amr6163@psu.edu>
2018-07-03 15:15:55 -0400

    Changed landing page to view locations

--- app/routes.js
+++ app/routes.js
@@ -39,7 +39,7 @@ module.exports = function(app, passport) {
 
     // process the login form
      app.post('/login', passport.authenticate('local-login', {
-       successRedirect : '/add-location', // redirect to the secure profile section.
+       successRedirect : '/view-locations', // redirect to the secure profile section.
        failureRedirect : '/login', // redirect back to the signup page if there is an error.
        failureFlash: true // allow flash messages.
      }));
